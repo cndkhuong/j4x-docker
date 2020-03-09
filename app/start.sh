9823b86092cc9373b0f13c772fd48ffbc6b169e6
Alexandre ELISÉ <developpeur@alexandre-elise.fr>
2020-03-09 01:29:50 -0400

    First commit

---
+++ app/start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# set repo
+if [ ! $REPO ];
+then
+  REPO='https://github.com/joomla/joomla-cms.git'
+fi;
+
+# set branch
+if [ ! $BRANCH ];
+then
+  BRANCH='4.0-dev'
+fi;
+
+# fetch joomla installation if project directory does not exist
+if [ ! -d /var/www/joomla-cms ];
+then
+  git clone $REPO --branch $BRANCH  --depth 1 /var/www/joomla-cms \
+  && cd /var/www/joomla-cms \
+  && rm -rf administrator/templates/atum/css \
+  && rm -rf templates/cassiopeia/css \
+  && rm -rf media/ \
+  && rm -rf node_modules/ \
+  && rm -rf libraries/vendor/
+fi;
+
+# execute required tasks for joomla installation if project directory exist
+if [ -d /var/www/joomla-cms ] && [ ! -f /var/www/joomla-cms/composer.lock ];
+then
+  chown -R www-data:www-data /var/www/joomla-cms \
+  && composer install --ignore-platform-reqs \
+  && npm ci \
+  && chown -R www-data:www-data /var/www/joomla-cms
+fi;
+
+service supervisor start
